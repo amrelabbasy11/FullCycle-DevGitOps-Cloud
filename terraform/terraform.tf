dd03a981345152d9ec6781c8cb483b0ec23e72d4
Amr Elabbasy <amr.elabbasy2003@gmail.com>
2025-09-02 09:26:52 +0300

    test Worflows22

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -26,14 +26,12 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "gitopsterrastate"
-    key    = "terraform.tfstate"
-    region = "us-east-2"
-  }
+#  backend "s3" {
+#    bucket = "gitopsterrastate"
+#    key    = "terraform.tfstate"
+#    region = "us-east-2"
+#  }
 
-  required_version = "~> 1.6.3"
+ required_version = "~> 1.6.3"
 }
-##
-##
-##
+
